16b1490c38dd257f2387a61e41b412b412c5379d
reason <reasonz@daum.net>
2022-09-23 18:57:50 +0900

    바닐라 자바스크립트 To do list

---
+++ todolist/README.md
@@ -0,0 +1,2 @@
+바닐라 자바스크립트로 만드는 TO DO LIST
+fetch API, json-server 사용하기
---
+++ todolist/script.js
@@ -0,0 +1,54 @@
+(function () {
+  'use strict';
+
+  const get = (target) => {
+    return document.querySelector(target);
+  };
+
+  const $todolist = get('.todos');
+
+  const createTodoElement = (todo) => {
+    const { id, title, completed } = todo;
+    console.log(todo);
+    console.log(id);
+    const $todoItem = document.createElement('div');
+    $todoItem.dataset.id = id;
+    $todoItem.classList.add('todo');
+    $todoItem.innerHTML = `
+      <input type="checkbox" id="checkbox" />
+      <span id="todo">${title}</span>
+      <label for="todo" style="display: none">${title}</label>
+      <div class="todo-buttons">
+        <button class="todo-edit-button">수정</button>
+        <button class="todo-delete-button">삭제</button>
+      </div>
+  `;
+
+    // 투두 엘리먼트 동적 생성
+    return $todoItem;
+  };
+
+  const API_URL = `http://localhost:3000/todos`;
+
+  const renderTodos = (todo) => {
+    todo.forEach((item) => {
+      const todoElement = createTodoElement(item);
+      $todolist.appendChild(todoElement);
+    });
+  };
+
+  const getTodos = () => {
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((todo) => {
+        renderTodos(todo);
+      })
+      .catch((error) => console.error(error));
+  };
+
+  const init = () => {
+    window.addEventListener('DOMContentLoaded', getTodos);
+  };
+
+  init();
+})();
