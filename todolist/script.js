d33fa2907445c975747be81052c15a58f3576185
reason <reasonz@daum.net>
2022-09-24 19:22:20 +0900

    todolist 취소 버튼

--- todolist/script.js
+++ todolist/script.js
@@ -16,11 +16,15 @@
     $todoItem.classList.add('todo');
     $todoItem.innerHTML = `
       <input type="checkbox" id="checkbox" />
-      <span id="todo">${title}</span>
-      <label for="todo" style="display: none">${title}</label>
+      <input id="todo-input" value=${title}>
+      <label id="todo-label" for="todo" >${title}</label>
       <div class="todo-buttons">
         <button class="todo-edit-button">수정</button>
         <button class="todo-delete-button">삭제</button>
+      </div>
+      <div class="modify-buttons">
+        <button class="todo-ok-button">확인</button>
+        <button class="todo-cancel-button">취소</button>
       </div>
   `;
 
@@ -38,6 +42,9 @@
   };
 
   const getTodos = () => {
+    const $formInput = $todoForm.querySelector('input');
+    $formInput.value = '';
+    $formInput.focus();
     $todolist.innerHTML = '';
 
     fetch(API_URL)
@@ -93,10 +100,50 @@
       .catch((error) => console.error(error));
   };
 
+  const modifyTodo = (e) => {
+    const $todo = e.target.closest('.todo');
+    const $todoInput = $todo.querySelector('#todo-input');
+    const $todoLabel = $todo.querySelector('label');
+    const todoValue = $todoInput.value;
+    const id = $todo.dataset.id;
+
+    const $todoButtons = $todo.querySelector('.todo-buttons');
+    const $modifyButtons = $todo.querySelector('.modify-buttons');
+
+    if (e.target.className === 'todo-edit-button') {
+      // 수정 버튼을 누르면 기존 수정, 삭제 버튼 안보이게 하고
+      // 확인, 취소 버튼 보이게 만들기.
+      // 확인 버튼 누르면 fetch로 값 업데이트시키기
+
+      $todoButtons.style.display = 'none';
+      $modifyButtons.style.display = 'block';
+      $todoLabel.style.display = 'none';
+      $todoInput.style.display = 'block';
+
+      $todoInput.value = '';
+      $todoInput.focus();
+      $todoInput.value = todoValue;
+    }
+
+    if (e.target.className === 'todo-ok-button') {
+      // 수정 확인 버튼 누르면
+    }
+
+    if (e.target.className === 'todo-cancel-button') {
+      console.log('ㅇ');
+      // 취소 버튼 누르면
+      $todoButtons.style.display = 'block';
+      $modifyButtons.style.display = 'none';
+      $todoLabel.style.display = 'block';
+      $todoInput.style.display = 'none';
+    }
+  };
+
   const init = () => {
     window.addEventListener('DOMContentLoaded', getTodos);
     $todoForm.addEventListener('submit', addTodo);
     $todosContainer.addEventListener('click', deleteTodo);
+    $todosContainer.addEventListener('click', modifyTodo);
   };
 
   init();
