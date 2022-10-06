6f2d38c74089be5196b287a86ac3f536b79335e2
reason <reasonz@daum.net>
2022-10-07 05:18:41 +0900

    Add header layout

--- pokemon/src/common/PageHeader.tsx
+++ pokemon/src/common/PageHeader.tsx
@@ -4,6 +4,11 @@ const PageHeader = () => {
   return (
     <Header>
       <Title>Pokémon</Title>
+      <Select>
+        <option value="official">Official</option>
+        <option value="A">A</option>
+        <option value="B">B</option>
+      </Select>
     </Header>
   );
 };
@@ -16,9 +21,20 @@ const Header = styled.nav`
 `;
 
 const Title = styled.h1`
-  font-size: 24px;
-  font-weight: bold;
+  font-size: 28px;
+  font-weight: 900;
   color: #ffcd03;
+  text-shadow: 2px -1px #3864a5, -1px 2px #3864a5, 2px 2px #3864a5, 2px 2px #3864a5;
+  margin-top: 3px;
+`;
+
+const Select = styled.select`
+  display: flex;
+  margin-left: auto;
+  padding: 8px 12px;
+  border-radius: 8px;
+  border: 1px solid #999;
+  color: #555;
 `;
 
 export default PageHeader;
