c6a364cff2d45b98e8e6fdda451af82b5684de2d
ReturnReason <reasonz@daum.net>
2022-11-05 22:15:24 +0900

    feat: Card 컴포넌트 추가

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,24 +1,23 @@
-import Nav from 'react-bootstrap/Nav';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
 import axios from 'axios';
 import { useEffect, useReducer } from 'react';
+import { Container, Row, Card, Button, Nav } from 'react-bootstrap';
 import styled from 'styled-components';
-import { Container, Row } from 'react-bootstrap';
 
-const GET_POKEMON_DATA = 'GET_POKEMON_DATA';
-const LOADING = 'LOADING';
-const ERROR = 'ERROR';
+export const GET_POKEMON_DATA = 'GET_POKEMON_DATA';
+export const LOADING = 'LOADING';
+export const ERROR = 'ERROR';
+export const GET_POKEMON_DETAIL = 'GET_POKEMON_DETAIL';
 
 const reducer = (state, action) => {
   switch (action.type) {
     case GET_POKEMON_DATA:
-      const oldData = [...state.pokemons];
       return {
         ...state,
-        pokemons: [...oldData, ...action.data],
+        pokemons: action.pokemons,
         loading: false,
       };
+    case GET_POKEMON_DETAIL:
+      return;
     case LOADING:
       return {
         ...state,
@@ -36,34 +35,107 @@ const reducer = (state, action) => {
 
 const initialState = {
   loading: false,
-  pokemons: [],
+  pokemons: {},
+};
+
+const POKEMON_URL = `https://pokeapi.co/api/v2/pokemon/`;
+
+// 포켓몬 이미지 파일 있는 API
+const fetchPokemonDetail = async (pokemonName) => {
+  try {
+    const pokemonDetailUrl = POKEMON_URL + `${pokemonName}`;
+    const res = await axios.get(pokemonDetailUrl);
+    const detail = res.data;
+
+    return {
+      id: detail.id,
+      name: detail.name,
+      // sprites: detail.sprites['official-artwork']['front_default'],
+    };
+  } catch (error) {
+    return console.error(error);
+  }
+};
+
+function PokeCard({ dispatch, pokemonName, pokemons }) {
+  useEffect(() => {
+    (async () => {
+      try {
+        const pokemonDetail = await fetchPokemonDetail(pokemonName);
+
+        dispatch({
+          type: GET_POKEMON_DETAIL,
+          data: pokemonDetail,
+        });
+      } catch (err) {
+        dispatch({
+          type: ERROR,
+        });
+        console.error(err);
+      }
+    })();
+  }, [dispatch, pokemonName]);
+
+  if (!pokemonName) return null;
+
+  return (
+    <Card key={`번째 `} style={{ width: '18rem' }}>
+      <img alt="포켓몬" />
+      <Card.Body>
+        <Card.Title>{pokemonName}</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the bulk of the card's
+          content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+// 포켓몬 API
+const fetchPokemonInfo = async () => {
+  try {
+    const result = await axios.get(POKEMON_URL);
+    return result.data;
+  } catch (error) {
+    return console.error(error);
+  }
 };
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  // 초기 포켓몬 데이터 받아오기
+
   useEffect(() => {
-    const poke = async () => {
-      dispatch({ type: LOADING });
+    console.log('111');
+
+    (async function fetchPokemon() {
+      // 로딩창 만들어주기
+      dispatch({
+        type: LOADING,
+      });
 
       try {
-        const res = await axios.get('https://pokeapi.co/api/v2/pokemon/');
+        const res = await fetchPokemonInfo();
+
         dispatch({
           type: GET_POKEMON_DATA,
-          data: res.data.results,
+          pokemons: res,
+          loading: false,
         });
-      } catch (err) {
-        console.error(err);
+      } catch (error) {
         dispatch({
-          tpye: ERROR,
+          type: ERROR,
         });
       }
-    };
-
-    poke();
+    })();
   }, []);
 
   const { pokemons, loading } = state;
+
+  console.log(loading);
   if (loading) return <div>로딩중</div>;
 
   return (
@@ -81,19 +153,15 @@ function App() {
       </Nav>
       <Container>
         <Row>
+          {/* // 비동기 데이터 받아오는거보다 더 빨리 실행돼서 오류 발생중 */}
           {pokemons.map((pokemon, idx) => {
             return (
-              <Card key={`${idx}번째 ${pokemon.name}`} style={{ width: '18rem' }}>
-                <img src={`${pokemon.url}`} alt="포켓몬" />
-                <Card.Body>
-                  <Card.Title>{pokemon.name}</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of the
-                    card's content.
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-              </Card>
+              <PokeCard
+                key={`${idx} ${pokemon.name}`}
+                dispatch={dispatch}
+                pokemonName={pokemon.name}
+                pokemons={pokemons}
+              />
             );
           })}
         </Row>
