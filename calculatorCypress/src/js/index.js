e7be3928a8117be3cec186cfd046e4c504093ff7
REASON <reasonz@daum.net>
2023-05-08 19:48:01 +0900

    feat: 계산기 숫자 버튼 클릭 이벤트 추가

--- calculatorCypress/src/js/index.js
+++ calculatorCypress/src/js/index.js
@@ -3,3 +3,19 @@ import Caculator from './components/Caclulator.js';
 document.addEventListener('DOMContentLoaded', () => {
   new Caculator();
 });
+
+const $ = (elem) => document.querySelector(elem);
+const $digits = $('.digits');
+const $total = $('#total');
+
+const handleDigitClick = (e) => {
+  if (e.target.classList.contains('digit')) {
+    $total.innerText = e.target.innerText;
+  }
+};
+
+function App() {
+  $digits.addEventListener('click', handleDigitClick);
+}
+
+App();
