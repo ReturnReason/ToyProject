7a40566db58d8902803b482da36ef2edf81363ac
REASON <reasonz@daum.net>
2023-05-17 00:46:41 +0900

    refactor: 계산기 디스플레이에 텍스트가 포함되어있는지 확인하는 중복되는 테스트 코드 checkDisplayValue 함수로 분리

--- calculatorCypress/cypress/e2e/calculator.cy.js
+++ calculatorCypress/cypress/e2e/calculator.cy.js
@@ -4,27 +4,31 @@ const clickDigitButtons = (digits = []) => {
   });
 };
 
+const checkDisplayValue = (value) => {
+  cy.get('#total').should('have.text', value);
+};
+
 describe('💙 계산기 앱 테스트', () => {
   beforeEach('✅ 페이지를 방문합니다.', () => {
     cy.visit('../../index.html');
   });
 
   it('✅ 디스플레이에 숫자 0이 표시됩니다.', () => {
-    cy.get('#total').should('have.text', 0);
+    checkDisplayValue(0);
   });
 
   it('✅ 숫자 버튼 클릭 시 디스플레이에 숫자가 나타납니다.', () => {
     clickDigitButtons(['1']);
-    cy.get('#total').should('have.text', 1);
+    checkDisplayValue(1);
   });
 
   it('✅ 숫자 버튼 2개를 클릭하면 디스플레이에 숫자 2개가 나타납니다.', () => {
     clickDigitButtons(['1', '2']);
-    cy.get('#total').should('have.text', 12);
+    checkDisplayValue(12);
   });
 
   it('✅ 3개의 숫자 버튼을 클릭하면 디스플레이에 숫자 3개가 나타납니다.', () => {
     clickDigitButtons(['1', '2', '3']);
-    cy.get('#total').should('have.text', 123);
+    checkDisplayValue(123);
   });
 });
