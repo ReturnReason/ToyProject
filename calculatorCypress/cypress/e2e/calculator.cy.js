7b48b6df39bdf2ce9a0b920f45a241546b1ba00a
REASON <reasonz@daum.net>
2023-05-30 00:28:48 +0900

    refactor: 연산자를 클릭하는 함수로 기능 분리

--- calculatorCypress/cypress/e2e/calculator.cy.js
+++ calculatorCypress/cypress/e2e/calculator.cy.js
@@ -8,6 +8,10 @@ const checkDisplayValue = (value) => {
   cy.get('#total').should('have.text', value);
 };
 
+const clickOperationButtons = (operation) => {
+  cy.get('.operation').contains(operation).click();
+};
+
 describe('💎 계산기 테스트', () => {
   beforeEach('페이지 방문', () => {
     cy.visit('../index.html');
@@ -34,9 +38,6 @@ describe('💎 계산기 테스트', () => {
 
   it('3개의 숫자 버튼(1, 2, 3)을 클릭하고 연산자 버튼을 누르면 디스플레이에 연산자가 표시된다.', () => {
     clickDigitButtons(['1', '2', '3']);
-    cy.get('.operation').contains('+').click();
+    clickOperationButtons('+');
     checkDisplayValue('123+');
   });
-  it('연산자 버튼을 연속해서 누르면 디스플레이에 마지막으로 누른 연산자만 표시한다.', () => {});
-  it('2번째 숫자를 입력하면 display에 2번째 숫자를 누적하여 표시한다.');
-});
